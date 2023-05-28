fabd9e3a9c03f725ad0aaa29cae7c0110fc10d4b
oswaldoarregoces <oswaldoarregoces@gmail.com>
2023-05-28 01:36:38 -0400

    modification 4

--- model.py
+++ model.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.naive_bayes import GaussianNB
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
